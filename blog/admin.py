08f4c09a7c3768e3cdd04924b8480014fc2a2343
Fani2000 <42602674+Fani2000@users.noreply.github.com>
2022-09-07 05:37:11 +0200

    Added the models, relationships, migrations and also liked the data from models or db to the front end site of things

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post,Author,Tag
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        "last_name","first_name","email_address"
+    )
+    
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('caption', )
+    
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date', )
+    list_filter = ("author", 'tags', "date")
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag, TagAdmin)
