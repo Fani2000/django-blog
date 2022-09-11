3b2f9c4d60ba383a0704c284bb6d94212ccc871a
Fani2000 <42602674+Fani2000@users.noreply.github.com>
2022-09-11 09:11:44 +0200

    Converted all the views into class Views, and also changed the urls to use views instead of functions

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="starting-page"),
-    path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post, name="post-details")
+    path('', views.IndexView.as_view(), name="starting-page"),
+    path("posts", views.PostsView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.PostView.as_view(), name="post-details")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,44 @@
 from django.shortcuts import render, get_object_or_404
-from datetime import date
 from .models import Post, Author, Tag
+from django.views import View
+from django.views.generic import ListView, TemplateView, DetailView
 
+# def index(request):
+#   latest_posts = Post.objects.all().order_by('date')[:3]
+#   return render(request, 'blog/index.html', {"posts":latest_posts})
 
+class IndexView(TemplateView):
+  template_name = "blog/index.html"
+  model = Post
 
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['posts'] = self.model.objects.all().order_by('date')[:3]
+    return context
 
-def index(request):
-  latest_posts = Post.objects.all().order_by('date')[:3]
-  return render(request, 'blog/index.html', {"posts":latest_posts})
+class PostsView(ListView):
+  template_name = "blog/all-posts.html"
+  model = Post
+  context_object_name = "posts"
 
-def posts(request):
-  sorted_posts = Post.objects.all().order_by('date')
-  return render(request, "blog/all-posts.html", {"posts": sorted_posts})
+# def posts(request):
+#   sorted_posts = Post.objects.all().order_by('date')
+#   return render(request, "blog/all-posts.html", {"posts": sorted_posts})
 
-def post(request, slug):
-  post = get_object_or_404(Post, slug=slug) 
-  # post = next(post for post in all_posts if post['slug'] == slug)
+class PostView(DetailView):
+  template_name = 'blog/post-details.html'
+  model = Post
+  context_object_name = "post"
 
-  return render(request, "blog/post-details.html", {"post": post, 'post_tags': post.tags.all()})
+  def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      post_object = get_object_or_404(self.model, slug=self.kwargs['slug'])
+      context['post_tags'] = post_object.tags.all()
+      return context
+  
+
+# def post(request, slug):
+#   post = get_object_or_404(Post, slug=slug) 
+#   # post = next(post for post in all_posts if post['slug'] == slug)
+
+#   return render(request, "blog/post-details.html", {"post": post, 'post_tags': post.tags.all()})
