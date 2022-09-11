71cbe6edca81694dd7410deb8de914c839789a98
Fani2000 <42602674+Fani2000@users.noreply.github.com>
2022-09-11 10:22:02 +0200

    Added the images upload into the blog site, also fixed the class Based views, also adjusted the settings, urls, and the models

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from .models import Post, Author, Tag
 from django.views import View
 from django.views.generic import ListView, TemplateView, DetailView
 
-# def index(request):
-#   latest_posts = Post.objects.all().order_by('date')[:3]
-#   return render(request, 'blog/index.html', {"posts":latest_posts})
-
-class IndexView(TemplateView):
+class IndexView(ListView):
   template_name = "blog/index.html"
   model = Post
+  context_object_name = 'posts'
 
-  def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-    context['posts'] = self.model.objects.all().order_by('date')[:3]
-    return context
+  def get_queryset(self):
+    query_set = super().get_queryset()
+    data = query_set[:3]
+    return data
+  
 
 class PostsView(ListView):
   template_name = "blog/all-posts.html"
   model = Post
+  ordering = ["-date"]
   context_object_name = "posts"
 
-# def posts(request):
-#   sorted_posts = Post.objects.all().order_by('date')
-#   return render(request, "blog/all-posts.html", {"posts": sorted_posts})
 
 class PostView(DetailView):
   template_name = 'blog/post-details.html'
@@ -32,13 +28,6 @@ class PostView(DetailView):
 
   def get_context_data(self, **kwargs):
       context = super().get_context_data(**kwargs)
-      post_object = get_object_or_404(self.model, slug=self.kwargs['slug'])
-      context['post_tags'] = post_object.tags.all()
+      context['post_tags'] = self.object.tags.all()
       return context
   
-
-# def post(request, slug):
-#   post = get_object_or_404(Post, slug=slug) 
-#   # post = next(post for post in all_posts if post['slug'] == slug)
-
-#   return render(request, "blog/post-details.html", {"post": post, 'post_tags': post.tags.all()})
